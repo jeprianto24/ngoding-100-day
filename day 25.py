156d688ca7a6d7f213c7bc9f786fd53fefdda26a
jeprianto24 <115442691+jeprianto24@users.noreply.github.com>
2022-11-12 23:13:03 +0800

    Add files via upload

---
+++ day 25.py
@@ -0,0 +1,9 @@
+#belajar percabangan python 
+
+angka = int(input("masukkan angka : "))
+
+if angka >= 0 :
+	print(angka,"adalah bilangan bulat positif")
+	
+else:
+	print(angka,"adalah bilangan bulat negatif")
