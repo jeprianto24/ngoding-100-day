7a3126c0f5614bfc6c0cd6e296514312567531ae
jeprianto24 <115442691+jeprianto24@users.noreply.github.com>
2022-11-15 23:30:22 +0800

    Add files via upload

---
+++ day 28.py
@@ -0,0 +1,8 @@
+#program menghitung waktu dalam sehari atau lebih
+
+hari =int(input("masukkan hari :"))
+jam = 24*hari
+menit = 60*jam
+detik = 60*menit
+
+print(f"{hari} hari,{jam} jam,{menit} menit,{detik} detik")
