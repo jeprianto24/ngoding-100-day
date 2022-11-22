dca9c1698091a639faac7c4e7507f69ae041405f
jeprianto24 <115442691+jeprianto24@users.noreply.github.com>
2022-11-22 22:54:43 +0800

    Add files via upload

---
+++ day 35.py
@@ -0,0 +1,26 @@
+while True:
+    print("="*40)
+    print("Komfersi Panjang Centimeter, Meter, Kilometer")
+    print("="*40)
+    print("""Pilih Satuan Panjang Awal
+    1. Cetimeter
+    2. Meter
+    3. Kilometer""")
+    pilih = input("Masukkan Pilihan : ")
+    if pilih == "1":
+        ukuran = int(input("\nMasukkan Ukuran Centimeter :"))
+        m = ukuran/100
+        k = ukuran/100000
+        print(f"{ukuran} cm = {m} m \n{ukuran} cm = {k}km\n")
+    elif pilih == "2":
+        ukuran = int(input("\nMasukkan Ukuran Meter :"))
+        cm = ukuran*100
+        k = ukuran/1000
+        print(f"{ukuran} m = {cm} cm \n{ukuran} m = {k}km\n")
+    elif pilih == "3":
+        ukuran = int(input("\nMasukkan Ukuran Kilometer :"))
+        cm = ukuran*100000
+        m = ukuran*1000
+        print(f"{ukuran} km = {cm} cm \n{ukuran} cm = {m}m\n")
+    else:
+        print("Pilihan Tidak Tersedia\n")
